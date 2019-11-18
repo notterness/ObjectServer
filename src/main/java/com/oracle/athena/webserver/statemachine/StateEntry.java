402ae17cb6dd57a766f4b46d1fabf86b88569fdc
psydow <patty.sydow@oracle.com>
2019-11-18 13:46:47 -0700

    This is the changes to the state machine as was discussed in the Thursday meeting.

--- src/main/java/com/oracle/athena/webserver/statemachine/StateEntry.java
+++ src/main/java/com/oracle/athena/webserver/statemachine/StateEntry.java
@@ -1,77 +1,13 @@
 package com.oracle.athena.webserver.statemachine;
 
-import java.util.ArrayList;
 
 /**
  * StateEntry - abstract class that describes the execution of this object - where it goes next, etc
  *
  * @param <T> - Object that is executing the state.
+ *
  */
-public abstract class StateEntry<T> {
-    private int entryNum;
-    private boolean blocks = false;
-    private ArrayList<StateEntryResult> results;
-    private int numResults;
+public abstract class StateEntry<T,R> {
 
-    public enum StateResultIndex {
-        STATE_RESULT_INDEX_OK(0),
-        STATE_RESULT_INDEX_ERROR(1),
-        STATE_RESULT_INDEX_WAIT(2),
-        STATE_RESULT_INDEX_MAX(3);
-
-        private final int value;
-
-        StateResultIndex(int value) {
-            this.value = value;
-        }
-
-        public int toInt() {
-            return this.value;
-        }
-    }
-
-    public StateEntry(int entryNum) {
-        StateResultIndex resultMax = StateResultIndex.STATE_RESULT_INDEX_MAX;
-
-        this.entryNum = entryNum;
-        this.numResults = 0;
-        this.results = new ArrayList<>(resultMax.toInt());
-    }
-
-    public abstract StateResultIndex execute(T t);
-
-    public abstract StateResultIndex complete(T t);
-
-    public boolean blocks() {
-        return this.blocks;
-    }
-
-    public void setBlocks(boolean blocks) {
-        this.blocks = blocks;
-    }
-
-    public int getEntryNum() {
-        return this.entryNum;
-    }
-
-    public void addResultEntry(StateEntryResult resultEntry, StateResultIndex index) {
-        try {
-            results.add(index.toInt(), resultEntry);
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("index greater than capacity: index " + index);
-            e.printStackTrace();
-        }
-        numResults++;
-    }
-
-    public StateEntryResult getResultEntry(StateResultIndex index) {
-        StateEntryResult result = null;
-        try {
-            result = results.get(index.toInt());
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("index greater than capacity: index " + index);
-            e.printStackTrace();
-        }
-        return result;
-    }
-}
+    public abstract StateQueueResult verbExecutor(T t);
+};
--- src/main/java/com/oracle/athena/webserver/statemachine/StateMachine.java
+++ src/main/java/com/oracle/athena/webserver/statemachine/StateMachine.java
@@ -1,70 +1,42 @@
 package com.oracle.athena.webserver.statemachine;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * StateMachine class: An array of StateEntry objects.
  *
- * @param <T> - any object that flows through a state machine.
+ * @param <K> - any object that flows through a state machine.
  */
-public class StateMachine<T> {
-    private ArrayList<StateEntry> stateList = new ArrayList<>();
+public class StateMachine<T,K> {
 
-    public void addStateEntry(StateEntry entry) {
-        stateList.add(entry);
+    private Map<K,StateEntry> stateTable = new HashMap<>();
+
+    public void addStateEntry(K verb, StateEntry entry) {
+        stateTable.put(verb, entry);
     }
 
-    public StateEntry getState(int stateNum) {
-        return stateList.get(stateNum);
-    }
-
-    public int getLastState() {
-        return this.stateList.size();
+    public StateEntry getState(K verb) {
+        return stateTable.get(verb);
     }
 
     /**
      * Method: stateMachineExecute - runs a StateTable until block or complete
      *
      * @param t              - object that is running the state machine.
-     * @param executingState - state to begin execution.
      * @return StateEntryResult - result
      */
-    public StateEntryResult stateMachineExecute(T t, int executingState) {
-        int stateIndex = executingState;
-        int nextIndex;
-        int lastIndex = this.getLastState();
-        StateEntryResult result;
-        StateEntry.StateResultIndex index;
+    public StateQueueResult stateMachineExecute( T t, K k) {
+        StateQueueResult result = StateQueueResult.STATE_RESULT_CONTINUE;
 
-        do {
-            StateEntry currentState = stateList.get(stateIndex);
-
-            index = currentState.execute(t);
-            result = currentState.getResultEntry(index);
-            if (result.getQueueResult() == StateQueueResult.STATE_RESULT_CONTINUE) {
-                stateIndex = result.getNextState();
-            }
-
-        } while (result.getQueueResult() == StateQueueResult.STATE_RESULT_CONTINUE);
-
-        return result;
-    }
-
-    /**
-     * Method: stateMachineCallbackComplete - executes the complete function of a blocking state. This state
-     * should advance the state executing position.
-     *
-     * @param t         - objext that contains the state machine
-     * @param nextIndex - next execution state.
-     * @return
-     */
-    public StateEntryResult stateMachineCallbackComplete(T t, int nextIndex) {
-        StateEntry entry = stateList.get(nextIndex);
-        StateEntry.StateResultIndex index;
-        StateEntryResult result;
-
-        index = entry.complete(t);
-        result = entry.getResultEntry(index);
+        try {
+            StateEntry state = stateTable.get(k);
+            result = state.verbExecutor(t);
+        } catch ( NullPointerException e) {
+            System.out.println("no state entry for key " + k);
+            e.printStackTrace();
+        }
         return result;
     }
 }
