d6b908f74b152e04a49ea1184c785861d125aa81
Mike McGuire <Mike.McGuire@oracle.com>
2019-11-21 11:52:09 -0700

    adding FIXMEs and TODOs to disambiguate the two and record identified problems in the code pending resolution

--- src/main/java/com/oracle/athena/webserver/server/ServerChannelLayer.java
+++ src/main/java/com/oracle/athena/webserver/server/ServerChannelLayer.java
@@ -22,24 +22,37 @@ abstract public class ServerChannelLayer implements Runnable {
     public static final int WORK_QUEUE_SIZE = 10;
 
     private int portNum;
+    //TODO: naming, should indicate it is the number of threads and not the threads themselves.
     int workerThreads;
+    //FIXME: this variable and its usage are not production ready.  We should not rely on trace statements in log files
+    //for anything in production.
     int serverClientId;
 
+    //FIXME: abstract class should not have a member variable it neither initializes nor uses. Probably delete this.
     public Thread serverAcceptThread;
+    //TODO: naming, either class ServerLoadBalancer or variable workHandler needs a new name (without server in it)
     ServerLoadBalancer serverWorkHandler;
 
     public MemoryManager memoryManager;
 
 
+    //FIXME: should become a local variable of the run method
     private boolean exitThreads;
 
+    //TODO: naming, should be called channel and cbThreadpool
     private AsynchronousServerSocketChannel serverChannel;
     private AsynchronousChannelGroup serverCbThreadpool;
 
+    //TODO: naming, should not be called server, name does not indicate purpose
+    //FIXME: this variable and its usage are not production ready.  We should not rely on trace statements in log files
+    //for anything in production.
     private int serverConnTransactionId;
 
+    //FIXME - remove or use instance variable
     private ByteBufferHttpParser byteBufferHttpParser;
 
+    //TODO: constructor parameters should have the same name as the instance variables to which they are assigned, and
+    // constructor code should read this.varname = varname
     public ServerChannelLayer(int numWorkerThreads, int listenPort, int clientId) {
         portNum = listenPort;
         workerThreads = numWorkerThreads;
@@ -59,6 +72,7 @@ abstract public class ServerChannelLayer implements Runnable {
      */
     public void stop() {
 
+        //FIXME: does not work with InitiatorServer
         serverWorkHandler.stop();
 
         /*
@@ -66,6 +80,7 @@ abstract public class ServerChannelLayer implements Runnable {
         try {
             serverChannel.close();
         } catch (IOException io_ex) {
+            //FIXME: handle this
             System.out.println("Unable to close server socket: " + serverConnTransactionId + " " + io_ex.getMessage());
         }
 
@@ -91,6 +106,7 @@ abstract public class ServerChannelLayer implements Runnable {
         try {
             serverCbThreadpool = AsynchronousChannelGroup.withFixedThreadPool(WORK_QUEUE_SIZE, Executors.defaultThreadFactory());
         } catch (IOException io_ex) {
+            //FIXME: handle this
             System.out.println("Unable to create server threadpool " + io_ex.getMessage());
             return;
         }
@@ -105,6 +121,7 @@ abstract public class ServerChannelLayer implements Runnable {
             serverChannel.bind(serverAddr);
 
         } catch (IOException e) {
+            //FIXME: handle this
             e.printStackTrace();
             return;
         }
@@ -129,10 +146,11 @@ abstract public class ServerChannelLayer implements Runnable {
 
                 if (!serverWorkHandler.startNewConnection(clientChan)) {
                     /*
-                     ** TODO: Need to return error to the client as there are no available ConnectionState objects
-                     ** to track this connection.
+                    FIXME: currently this only happens if the load balancer was unable to allocate a connection pool
+                    from the unreserved normal pool, and also unable to allocate a connection pool from the unreserved
+                    blocking pool. This probably shouldn't be passed up to us via a boolean return type, and depending
+                    on the solution we design, might want to be handled at a lower layer.
                      */
-
                 }
             } catch (ExecutionException | InterruptedException ex) {
                 System.out.println("Server run(): accept error: " + ex.getMessage());
