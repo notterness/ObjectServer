306ccf5dc6919a017be99a7bbe9b45527da01465
Noel Otterness <noel.otterness@oracle.com>
2019-12-02 12:10:10 -0700

    new file:   src/main/java/com/oracle/athena/webserver/server/BlockingPipelineThreadPool.java 	new file:   src/main/java/com/oracle/athena/webserver/server/BlockingWorkerThread.java 	modified:   src/main/java/com/oracle/athena/webserver/server/ComputeThread.java 	modified:   src/main/java/com/oracle/athena/webserver/server/ServerLoadBalancer.java

---
+++ src/main/java/com/oracle/athena/webserver/server/BlockingWorkerThread.java
@@ -0,0 +1,76 @@
+package com.oracle.athena.webserver.server;
+
+import com.oracle.athena.webserver.connectionstate.ConnectionState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/*
+** These are threads used to run Pipelines that are accessing off box resources that would block the
+**   ServerWorkerThreads.
+ */
+class BlockingWorkerThread implements Runnable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BlockingWorkerThread.class);
+
+    private final int blockingThreadId;
+
+    private volatile boolean running;
+    private Thread blockingThread;
+
+    private final BlockingQueue<ConnectionState> blockingWorkQ;
+
+
+    BlockingWorkerThread(final BlockingQueue<ConnectionState> workQueue, final int blockingThreadId) {
+
+        this.blockingWorkQ = workQueue;
+        this.blockingThreadId = blockingThreadId;
+    }
+
+    void start() {
+        running = true;
+        blockingThread = new Thread(this);
+        blockingThread.start();
+    }
+
+    void stop() {
+        running = false;
+
+        try {
+            blockingThread.join(1000);
+        } catch (InterruptedException int_ex) {
+            System.out.println("BlockingWorkerThread[" + blockingThreadId + "] failed: " + int_ex.getMessage());
+        }
+    }
+
+    /*
+     ** This is the method that does the actual work for this thread. It handles multiple
+     **   connections at once. The expectation is that the pipelines that are using this thread
+     **   are running operations that are blocking.
+     */
+    public void run() {
+
+        LOG.info("BlockingWorkerThread[" + blockingThreadId + "] start " + Thread.currentThread().getName());
+
+        while (running) {
+            ConnectionState work;
+
+            try {
+                work = blockingWorkQ.poll(100, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException int_ex) {
+                LOG.warn("BlockingWorkerThread[" + blockingThreadId + "] " + int_ex.getMessage());
+                work = null;
+                running = false;
+            }
+
+            if (work != null) {
+                work.stateMachine();
+            }
+        }
+
+        LOG.info("BlockingWorkerThread[" + blockingThreadId + "] exit " + Thread.currentThread().getName());
+    }
+
+}
