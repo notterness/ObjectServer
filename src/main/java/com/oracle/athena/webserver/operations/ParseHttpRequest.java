6ae2a485bb13524b465adbc3c99f53c831a15905
Noel Otterness <noel.otterness@oracle.com>
2019-12-15 18:20:31 -0700

    modified:   src/main/java/com/oracle/athena/webserver/buffermgr/BufferManagerPointer.java 	modified:   src/main/java/com/oracle/athena/webserver/operations/ParseHttpRequest.java

--- src/main/java/com/oracle/athena/webserver/operations/ParseHttpRequest.java
+++ src/main/java/com/oracle/athena/webserver/operations/ParseHttpRequest.java
@@ -1,17 +1,36 @@
 package com.oracle.athena.webserver.operations;
 
+import com.oracle.athena.webserver.buffermgr.BufferManager;
+import com.oracle.athena.webserver.buffermgr.BufferManagerPointer;
+import com.oracle.athena.webserver.connectionstate.BufferState;
+
 public class ParseHttpRequest implements Operation {
+
+    BufferManager bufferManager;
+    BufferManagerPointer httpBufferPointer;
 
     public void initialize() {
 
+        /*
+        ** Register this as dependent upon the read Operation registration
+         */
     }
 
     public void eventHandler() {
 
+        /*
+        ** Add this to the execute queue if it is not already on it.
+         */
     }
 
     public void execute() {
+        BufferState bufferState;
 
+        while ((bufferState = bufferManager.poll(httpBufferPointer)) != null) {
+            /*
+            ** Now run the Buffer State through the Http Parser
+             */
+        }
     }
 
     public void complete() {
